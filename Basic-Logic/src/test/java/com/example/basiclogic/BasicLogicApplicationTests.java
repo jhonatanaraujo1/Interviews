5a5b127f61ffd803c06008a832e570997978c96f
Jhonatan Sousa <jhonatan.araujo1@hotmail.com>
2021-11-18 19:38:33 -0300

    Basic logic for programming sums and multiplications

---
+++ Basic-Logic/src/test/java/com/example/basiclogic/BasicLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basiclogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicLogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
